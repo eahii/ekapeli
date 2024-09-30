a6029263450869e83157d968eb1704b6512c9638
eahii <142023232+eahii@users.noreply.github.com>
2024-09-30 10:50:39 +0300

    Add files via upload

---
+++ eka-peli/scripts/coin.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	animation_player.play("PicupAnimation")
